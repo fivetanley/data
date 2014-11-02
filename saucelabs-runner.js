3d7060b0f6539995a3f800b29ff69749ca3d0697
Stanley Stuart <stanley@stan.li>
2014-11-02 13:35:29 -0600

    add saucelabs runner

---
+++ saucelabs-runner.js
@@ -0,0 +1,30 @@
+var TestRunner = require('grunt-saucelabs/src/TestRunner');
+var request = require('request');
+
+var BROWSERS = [
+  {
+    platform: 'Windows 8.1',
+    version: '',
+    browserName: 'Chrome'
+  }
+];
+
+console.log(process.env.SAUCE_USERNAME)
+
+var runner = new TestRunner({
+  username: process.env.SAUCE_USERNAME,
+  key: process.env.SAUCE_ACCESS_KEY,
+  urls: ['http://builds.emberjs.com/lolfivetanley/saucelabs.html'],
+  browsers: BROWSERS
+}, 'qunit', function(){});
+
+var done = function(result){
+}
+
+
+runner.runTests().then(done).
+  then(function(){
+  console.log('done');
+}, function(err){
+  console.log('err', err);
+});
